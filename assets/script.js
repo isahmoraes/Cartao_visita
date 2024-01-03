4f2cb2e6c6e92d632ac4eecd012106d8a060c95b
isadora.moraes <isahmoraes8@gmail.com>
2024-01-03 20:29:19 -0300

    Projeto final alteração js

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 function toggleMode() {
   const html = document.documentElement
-   
+  html.classList.toggle("light")
   const img=document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    html.classList.remove("light")
+   
     img.setAttribute("src", "./assets/avatar.png")
   } else {
-    html.classList.add("light") 
-    
+     
     img.setAttribute("src", "./assets/avatar-light-mode.png")
   }
 }
