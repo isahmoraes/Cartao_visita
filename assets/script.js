2dc4ec9a88b92c321bdb1b451210cbb30078d9bf
isadora.moraes <isahmoraes8@gmail.com>
2024-01-04 21:08:55 -0300

    teste de light

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
+  
   const img=document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
